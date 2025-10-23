5eeeea07e6634c4a7a9271923e0e7a267add16f6
Ashley Henry <ashley.brayman.usn@gmail.com>
2025-10-23 18:42:59 -0400

    Replace emoji hearts with SVG icon that changes color on favorite toggle

--- _extensions/favorites/favorites-button.lua
+++ _extensions/favorites/favorites-button.lua
@@ -49,7 +49,9 @@ function Meta(meta)
     local button_html = string.format([[
       <div class="favorites-button-container">
         <button id="favorites-button" class="favorites-button" data-page-info='%s' title="Add to Favorites">
-          <span class="favorites-icon">♡</span>
+          <svg class="favorites-icon" viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+          </svg>
         </button>
       </div>
     ]], page_info)
