44d9c9530b41b8404c4bf172a0e14fe3a80780b8
Ashley Henry <ashley.brayman.usn@gmail.com>
2025-10-22 20:32:02 -0400

    working commit - functional

--- _extensions/favorites/favorites-button.lua
+++ _extensions/favorites/favorites-button.lua
@@ -1,6 +1,24 @@
 -- favorites-button.lua
 -- This filter adds a favorites button to each page
 
+-- Simple function to create JSON string manually (no dependency on pandoc.utils.to_json)
+function json_encode(val)
+  if type(val) == "string" then
+    return '"' .. val:gsub('"', '\\"'):gsub("\n", "\\n") .. '"'
+  elseif type(val) == "table" then
+    local json = "{"
+    local first = true
+    for k, v in pairs(val) do
+      if not first then json = json .. "," end
+      first = false
+      json = json .. '"' .. k .. '":' .. json_encode(v)
+    end
+    return json .. "}"
+  else
+    return tostring(val)
+  end
+end
+
 -- Function to handle metadata and format title
 function Meta(meta)
   -- Only process if we're in HTML format
@@ -25,7 +43,7 @@ function Meta(meta)
     end
 
     -- Create page info as JSON
-    local page_info = pandoc.utils.to_json({title = title})
+    local page_info = json_encode({title = title})
 
     -- Create the favorites button HTML
     local button_html = string.format([[
